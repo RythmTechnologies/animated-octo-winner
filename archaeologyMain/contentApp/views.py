141e46c577182175b43a1919a977fcafa6456c88
CoderMungan <codermungan@gmail.com>
2023-12-09 00:57:51 +0300

    Fix: typehint syntax

--- archaeologyMain/contentApp/views.py
+++ archaeologyMain/contentApp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+#For TypeHint
+import typing as t
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+RedirectOrResponse = t.Union[HttpResponseRedirect, HttpResponse]
+
+
 # Create your views here.
 
 
-def get_index(request):
+def get_index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html')
