c0499ee805e226f3cad42dbcdae8fc2f22e32865
CoderMungan <codermungan@gmail.com>
2023-12-09 01:06:18 +0300

    Add: typehint

--- archaeologyMain/userApp/views.py
+++ archaeologyMain/userApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+#For TypeHint
+import typing as t
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+RedirectOrResponse = t.Union[HttpResponseRedirect, HttpResponse]
+
+
+# Example typehint func
+"""
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'templatename.html')
+"""
