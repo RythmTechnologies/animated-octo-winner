31e1faebd61b750dd563d5ec2a23cce803898ea9
CoderMungan <codermungan@gmail.com>
2023-12-12 01:23:09 +0300

    feat: add authentications & dashboard & notfound

--- archaeologyMain/contentApp/urls.py
+++ archaeologyMain/contentApp/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 
+from .views import *
+
 
 urlpatterns = [
-    path(''),
+    path('dashboard', get_dashboard, name='dashboard'),
 ]
--- archaeologyMain/contentApp/views.py
+++ archaeologyMain/contentApp/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
 
 #For TypeHint
 import typing as t
@@ -6,8 +12,47 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 RedirectOrResponse = t.Union[HttpResponseRedirect, HttpResponse]
 
 
-# Create your views here.
 
+# Homepage start
 
-def get_index(request: HttpRequest) -> HttpResponse:
+def get_index(request: HttpRequest) -> RedirectOrResponse:
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username and password:
+            userLogin = authenticate(request, username = username, password = password)
+
+            if userLogin is not None:
+                login(request, userLogin)
+
+                # Message Success
+                messages.success(request, "Login Correct!")
+                return redirect('dashboard')
+            else:
+
+                # Message Error
+                messages.error(request, 'The username or password is incorrect')
+                return redirect('homepage')
+        else:
+
+            # Message Warning
+            messages.warning(request, 'Please fill in the fields.')
+            return redirect('homepage')
+
     return render(request, 'index.html')
+
+# Homepage End
+
+# Dashboard Start
+@login_required(login_url='homepage')
+def get_dashboard(request: HttpRequest) -> HttpResponse:
+    return render(request, 'dashboard.html')
+# Dashboard End
+
+
+# 404 Page Start
+def get_notFound(request: HttpRequest) -> HttpResponse:
+    return render(request, '404page.html')
+# 404 Page End
