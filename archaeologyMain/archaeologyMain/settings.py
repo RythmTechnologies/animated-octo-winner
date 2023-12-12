d2761deed1c3d40040d055dee9d8f934dce809dc
AysKrimn <akabilmemne@gmail.com>
2023-12-12 21:04:02 +0300

    feat: add base user  model

--- archaeologyMain/archaeologyMain/settings.py
+++ archaeologyMain/archaeologyMain/settings.py
@@ -121,6 +121,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+# USER MODEL
+AUTH_USER_MODEL = "userApp.SiteUser"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
--- archaeologyMain/userApp/models.py
+++ archaeologyMain/userApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+# types : admin, moderator, ogrenci
+class SiteUser(AbstractUser):
+    
+    isModerator = models.BooleanField(("Moderatör mi"), default=False)
+    isStudent = models.BooleanField(("Öğrenci mi"), default=False)
+
+
+    def __str__(self) -> str:
+        return self.username
+    
