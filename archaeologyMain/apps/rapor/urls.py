55de2c57c564f5fdf89e95b0da03d3d5fa492e23
CoderMungan <codermungan@gmail.com>
2024-01-15 20:24:28 +0300

    feat: crud done from rapor

--- archaeologyMain/apps/rapor/urls.py
+++ archaeologyMain/apps/rapor/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path("ekle", get_rapor, name="set-rapor"),
     path("liste", get_rapor_list, name="rapor-liste"),
-    path("sil/<id>", delete_rapor, name="delete-rapor")
+    path("delete/<id>", delete_rapor, name="delete-rapor"),
+    path("update/<id>", update_rapor, name="update-rapor")
 ]
