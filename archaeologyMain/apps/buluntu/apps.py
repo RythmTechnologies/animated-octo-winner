b1ed3a9e3dac65af982f93b25bd6536009cecf40
AysKrimn <akabilmemne@gmail.com>
2024-02-04 05:03:06 +0300

    feat: create default values for buluntu models

--- archaeologyMain/apps/buluntu/apps.py
+++ archaeologyMain/apps/buluntu/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BuluntuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.buluntu'
+
+
+    def ready(self):
+        import apps.buluntu.signals
+
+    
